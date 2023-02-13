33cc2d5c268e1c0f4b3e4300c0ecb625404e3bfd
Akshaykumar Maldhure <akshaykumar.maldhure@gmail.com>
2023-02-13 16:53:29 +0530

    added comments

--- app.py
+++ app.py
@@ -4,24 +4,32 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+# DB Connection
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/todoapp'
 mongo = PyMongo(app)
 
 todos_collection = mongo.db.todos
 
+
+# list all todos api
 @app.route('/')
 def index():
-    todos = todos_collection.find().sort("_id",-1)
+    todos = todos_collection.find().sort("_id", -1)
     return render_template('index.html', todos=todos)
 
+
+# add new todos
 @app.route('/add_todo', methods=['POST'])
 def add_todo():
-    todo_item = request.form.get('add-todo')
-    todos_collection.insert_one({'text' : todo_item})
+    todo_item = request.form.get('name')
+    # add an item if name is not blank
+    if todo_item.strip() != "":
+        todos_collection.insert_one({'name': todo_item})
     return redirect(url_for('index'))
 
+
+# delete a todos by id
 @app.route('/delete_todo/<id>')
 def delete_todo(id):
-    print(id)
     todos_collection.delete_one({'_id': ObjectId(id)})
     return redirect(url_for('index'))
